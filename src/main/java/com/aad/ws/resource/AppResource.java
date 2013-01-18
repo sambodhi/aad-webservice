68fec2f16f87e0f35d830acb3c15cf23ea169234
Marios Constantinides <co.marios@gmail.com>
2013-01-18 18:25:24 +0000

    Test commit

--- src/main/java/com/aad/ws/resource/AppResource.java
+++ src/main/java/com/aad/ws/resource/AppResource.java
@@ -33,7 +33,7 @@ public class AppResource {
 		logger.debug("Resource >> App >> getAppDetails >> param: id= "+id);
 		AppDetails appDetails = new AppDetails();
 		//stub
-		appDetails.setName("survey");
+		appDetails.setName(id);
 		appDetails.setDescription("blah blah");
 		appDetails.setSize("50 MB");
 		return appDetails;
