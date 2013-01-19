bf61d54d3c9f74ba52a2927fa5b0d1f2faaa6279
Marios Constantinides <co.marios@gmail.com>
2013-01-19 14:50:45 +0000

    These are new changes

--- src/main/java/com/aad/ws/resource/AppResource.java
+++ src/main/java/com/aad/ws/resource/AppResource.java
@@ -1,5 +1,7 @@
 package com.aad.ws.resource;
 
+import java.io.File;
+import java.util.ArrayList;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -15,28 +17,55 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
- 
-@Component  
-//@Scope("request") 
+@Component
+// @Scope("request")
 @Path("/application")
 public class AppResource {
-	
+
 	@Autowired
 	private AppService appService;
-	
+	private static ArrayList<String> files = new ArrayList<String>();
 	private static final Logger logger = Logger.getLogger(AppResource.class);
+	private static final String APPSDIR = "C:\\Users\\MConstantinides\\git\\aad-webservice\\Apps";
 	
 	@GET
 	@Path("details/{appId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public AppDetails getAppDetails(final @PathParam("appId") String id) {
-		logger.debug("Resource >> App >> getAppDetails >> param: id= "+id);
-		AppDetails appDetails = new AppDetails();
-		//stub
-		appDetails.setName(id);
-		appDetails.setDescription("blah blah");
-		appDetails.setSize("50 MB");
+	public ArrayList<AppDetails> getAppDetails(final @PathParam("appId") String id) {
+		logger.debug("Resource >> App >> getAppDetails >> param: id= " + id);
+		ArrayList<AppDetails> appDetails = new ArrayList<AppDetails>();
+
+		printFnames(APPSDIR);
+		String desc = "";
+
+		for (int i = 0; i < files.size(); i++) {
+
+			AppDetails ap = new AppDetails();
+			ap.setName(files.get(i));
+			ap.setDescription("test");
+			ap.setSize("test1");
+			appDetails.add(ap);
+		
+		}
+
+		// stub
+		//appDetails.setName(id);
+		//appDetails.setDescription(desc);
+		//appDetails.setSize("50 MB");
 		return appDetails;
 	}
- 
+
+	public static void printFnames(String sDir) {
+		File[] faFiles = new File(sDir).listFiles();
+
+		for (File file : faFiles) {
+			if (file.getName().matches("^(.*?)")) {
+				files.add(file.getAbsolutePath());
+				// System.out.println(file.getAbsolutePath());
+			}
+			if (file.isDirectory()) {
+				printFnames(file.getAbsolutePath());
+			}
+		}
+	}
 }
