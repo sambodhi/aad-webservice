84b185b3b945e71daab6ce4d4d1dadad57aff68d
milochen89 <milochen89@yahoo.com>
2013-01-30 17:05:10 +0000

    add submitResult function

--- src/main/java/com/aad/ws/resource/ResultResource.java
+++ src/main/java/com/aad/ws/resource/ResultResource.java
@@ -1,5 +1,41 @@
 package com.aad.ws.resource;
 
-public class ResultResource {
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.aad.ws.dao.ResultDAO;
+import com.aad.ws.domain.Results;
+import com.aad.ws.service.AppService;
+import com.aad.ws.service.submitResultService;
+
+
+
+@Component
+@Path("/result")
+public class ResultResource {
+	
+	@Autowired
+	private submitResultService resultService;
+	private static final Logger logger = Logger.getLogger(ResultResource.class);
+	
+	@POST
+	@Path("submit")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response StoreInDB(Results results) {
+		String rere = "Results saved : " + results;
+		logger.debug("*******************"+ results.gettotalquestions());
+		
+		results=resultService.subResults(results);
+		
+		return Response.status(201).entity(results).build();
+	}
 }
