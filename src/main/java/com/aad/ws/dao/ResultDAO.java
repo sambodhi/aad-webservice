136af6dcc2a2f8c3f9485ef7cb53780cab814f39
Marios Constantinides <co.marios@gmail.com>
2013-02-25 11:52:19 +0000

    Hao's code

--- src/main/java/com/aad/ws/dao/ResultDAO.java
+++ src/main/java/com/aad/ws/dao/ResultDAO.java
@@ -2,8 +2,9 @@ package com.aad.ws.dao;
 
 import java.util.List;
 import com.aad.ws.domain.Results;
+import com.aad.ws.exception.InvalidAttribute;
 
 public interface ResultDAO {
-	Results submitResult(Results res);
+	Results submitResult(Results res) throws InvalidAttribute;
 }
 
